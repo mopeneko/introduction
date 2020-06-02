8ae108cb990b0e0260138d5ead9741056bc18d97
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-06-02 19:18:26 +0900

    Prettierを実行

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/js/app.js',
+  entry: "./src/js/app.js",
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public/assets/js')
+    filename: "bundle.js",
+    path: path.join(__dirname, "public/assets/js"),
   },
 
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              sourceMap: true
-            }
-          }
-        ]
-    }]
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '../css/bundle.css'
-    })
+      filename: "../css/bundle.css",
+    }),
   ],
 
   devServer: {
-      contentBase: path.resolve(__dirname, 'public')
-  }
+    contentBase: path.resolve(__dirname, "public"),
+  },
 };
